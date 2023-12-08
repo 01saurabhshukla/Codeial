1ac71f6474444da4df9e26a928cc773b852c5cb4
Saurabh Shukla <103saurabhshukla@gmail.com>
2023-12-08 23:55:29 +0530

    add controller and router to home

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// This one should load before server starts
+// using express routers
+app.use('/', require('./routes'));
+
 
 app.listen(port,function(err){
     if(err){
