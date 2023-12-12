b47bc1933f1604f0d5b2c2cdd81cc3b9cca24369
Saurabh Shukla <103saurabhshukla@gmail.com>
2023-12-13 00:37:13 +0530

    done steps 1: showing post 2:Populating user details 3:Showing user details

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,6 @@ const homeController = require('../controllers/home_controller');
 
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
-router.use('/post',require('./post'));
+router.use('/posts',require('./posts'));
 
 module.exports = router;
