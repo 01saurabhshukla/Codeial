9cbbf64191450318ee38cdf8ec0fe36162f58abb
Saurabh Shukla <103saurabhshukla@gmail.com>
2024-01-26 13:02:09 +0530

    dynamic addition of posts is done

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,3 +1,83 @@
 {
-    
+    // method to submit the form data for new post using AJAX
+    let createPost = function(){
+        let newPostForm = $("#new-post-form");
+
+        newPostForm.submit(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'post',
+                url: '/posts/create',
+                data: newPostForm.serialize(),
+                success: function(data){
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button',newPost));
+                }, error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+        });
+    }
+
+
+    // method to create a post in dom
+    let newPostDom = function(post){
+        return $(`
+        <li id="post-${ post._id }">
+        <p>
+          
+          <small>
+            <a class="delete-post-button" href="/posts/destroy/${ post._id }">X</a>
+          </small>
+          
+          ${post.content}
+          <br />
+          ${post.user.name }
+        </p>
+        <div class="post-comments">
+        
+          <form action="/comments/create" method="POST">
+            <input
+              type="text"
+              name="content"
+              placeholder="Type Here To Add Comment..."
+              required
+            />
+            <input type="hidden" name="post" value=" ${post._id}" />
+            <input type="submit" value="Add Comment" />
+          </form>
+
+        </div>
+        <div class="post-comments-list">
+          <ul id="post-comments- ${post._id}">
+            
+          </ul>
+        </div>
+      </li>`)
+    }
+
+
+
+    // method to delete post from DOM
+    // deleteLink = it is a link of anchor tag frovided to delete 
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                }, error: function(error){
+                    console.log(error.responseText);
+                }
+            })
+        })
+    }
+
+
+    createPost();
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -5,12 +5,22 @@ module.exports.create = async function (req, res) {
 
   try {
 
-    await Post.create({
+    
+    let post = await Post.create({
 
       content: req.body.content,
       user: req.user._id,
 
     });
+
+    if(req.xhr){
+      return res.status(200).json({
+        data: {
+          post: post
+        },
+        message: "Post Created Succesfully"
+      })
+    }
 
     req.flash('success','Post published!');
     return res.redirect("back");
@@ -34,6 +44,15 @@ module.exports.destroy = async function (req, res) {
       await Comment.deleteMany({
         post: req.params.id,
       });
+
+      if(req.xhr){
+        return res.status(200).json({
+          data: {
+            post_id: req.params.id
+          },
+          message: "Post deleted"
+        })
+      }
 
       req.flash('success','Post and associated comments are deleted');
 
